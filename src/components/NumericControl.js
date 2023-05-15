4faf5e75c5fef084fc0472e6df5e5b0f536f4b47
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-14 21:23:35 -0700

    Fix layout when on smaller screen

--- src/components/NumericControl.js
+++ src/components/NumericControl.js
@@ -10,36 +10,38 @@ export default function NumericControl({ label, value, name, step, onChange }) {
   };
 
   return (
-    <div>
+    <>
       <label
         htmlFor={value}
-        className="mr-2 text-light-heading dark:text-dark-heading"
+        className="block mr-2 text-light-heading dark:text-dark-heading"
       >
         {label}
       </label>
-      <button
-        type="button"
-        onClick={handleDecrement}
-        className="w-10 h-10 bg-red-500 text-white dark:bg-red-600 dark:text-white"
-      >
-        -
-      </button>
-      <input
-        type="number"
-        id={name}
-        name={name}
-        value={value}
-        onChange={(event) => onChange(event)}
-        required
-        className="py-2  text-center"
-      />
-      <button
-        type="button"
-        onClick={handleIncrement}
-        className="w-10 h-10 bg-green-500 text-white dark:bg-green-600 dark:text-white"
-      >
-        +
-      </button>
-    </div>
+      <div className="flex items-center">
+        <button
+          type="button"
+          onClick={handleDecrement}
+          className="w-10 h-10 bg-red-500 text-white dark:bg-red-600 dark:text-white"
+        >
+          -
+        </button>
+        <input
+          type="number"
+          id={name}
+          name={name}
+          value={value}
+          onChange={(event) => onChange(event)}
+          required
+          className="py-2  text-center w-full"
+        />
+        <button
+          type="button"
+          onClick={handleIncrement}
+          className="w-10 h-10 bg-green-500 text-white dark:bg-green-600 dark:text-white"
+        >
+          +
+        </button>
+      </div>
+    </>
   );
 }
