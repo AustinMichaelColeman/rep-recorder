877a773041d925fe19e5bb246033d2780c2c8a1c
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-05-14 22:39:31 -0700

    Remove unnecessary div

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,14 @@ import styles from "@/app/globals.css";
 export default function Home() {
   return (
     <main className="flex flex-col bg-light-background dark:bg-dark-background min-h-screen">
-      <div>
-        <h1 className="text-4xl font-bold text-light-heading dark:text-dark-heading text-center">
-          Rep Recorder
-        </h1>
-        <h2 className="text-2xl font-medium text-light-heading dark:text-dark-heading text-center">
-          A workout tracking website
-        </h2>
-        <Credits />
-        <WorkoutTracker className="flex-grow" />
-      </div>
+      <h1 className="text-4xl font-bold text-light-heading dark:text-dark-heading text-center">
+        Rep Recorder
+      </h1>
+      <h2 className="text-2xl font-medium text-light-heading dark:text-dark-heading text-center">
+        A workout tracking website
+      </h2>
+      <Credits />
+      <WorkoutTracker className="flex-grow" />
     </main>
   );
 }
